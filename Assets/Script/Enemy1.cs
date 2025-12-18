e0198ea4dc17115430c8e46f26b8b6894ce6c5ce
iwamotorikuto <ca01254003@st.kawahara.ac.jp>
2025-12-18 14:01:34 +0900

    UIの配置 (#10)

--- Assets/Script/Enemy1.cs
+++ Assets/Script/Enemy1.cs
@@ -1,18 +1,65 @@
+﻿using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy1 : MonoBehaviour
 {
+    public Image healthImage;
+    public int maxHealth = 100;
+    public int currentHealth;
+    private int hp;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hp = maxHealth;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            damage(10);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            heal(5);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //当たったのがプレイヤー
+        if(other.gameObject.CompareTag("Bird4_Cyan"))
+        {
+
+            damage(10);
+        }
+    }
+
+    //void Damage(int damage)
+    //{
+    //    hp -= damage;
+    //    if(hp <= 0)
+    //    {
+    //        hp = 0;
+    //    }
+    //}
+
+    public void damage(int damage)
+    {
+        hp -= damage;
+        healthImage.fillAmount = (float)hp / maxHealth;
+    }
+
+    public void heal(int heal)
+    {
+        hp += heal;
+        healthImage.fillAmount = (float)hp / maxHealth;
     }
 }
