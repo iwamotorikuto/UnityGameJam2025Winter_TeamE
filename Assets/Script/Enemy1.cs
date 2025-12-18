7d374051b06a612409b6c39fb374b21a3126e192
iwamotorikuto <ca01254003@st.kawahara.ac.jp>
2025-12-18 10:47:19 +0900

    EnemyのHP処理とHPバーのUI配置の追加

--- Assets/Script/Enemy1.cs
+++ Assets/Script/Enemy1.cs
@@ -1,18 +1,43 @@
+﻿using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy1 : MonoBehaviour
 {
+    public int maxHealth = 100;
+    public int currentHealth;
+    private int hp;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hp = maxHealth;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //当たったのがプレイヤー
+        if(other.gameObject.CompareTag("Bird4_Cyan"))
+        {
+            Damage(10);
+        }
+    }
+
+    void Damage(int damage)
+    {
+        hp -= damage;
+        if(hp <= 0)
+        {
+            hp = 0;
+        }
     }
 }
