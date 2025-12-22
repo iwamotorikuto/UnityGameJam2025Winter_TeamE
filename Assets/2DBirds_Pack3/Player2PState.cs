e9c393afced0d196414e8939f672d1e3d87a3129
AokiMutsuki <ca01254001@st.kawahara.ac.jp>
2025-12-22 14:08:25 +0900

    Aoki/player2P (#42)
    確認しました。

---
+++ Assets/2DBirds_Pack3/Player2PState.cs
@@ -0,0 +1,40 @@
+﻿///
+/// 
+/// プレイヤー２の体力を管理するスクリプト
+/// 
+/// 
+using UnityEngine;
+
+public class Player2PState : MonoBehaviour
+{
+    [Header("プレイヤー２の最大体力"), SerializeField]
+    private float maxHealth = 100;
+
+    [Header("プレイヤー２の現時点の体力"), SerializeField]
+    private float currentHealth;
+
+   
+    void Start()
+    {
+        currentHealth = 100;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //当たり判定でダメージを受ける
+    private void OnCollisionEnter2D(Collision2D collision)
+    { 
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    private void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
+}
